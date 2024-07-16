860ce281c1bafeb0cd1a264abd1a7234ebaf9b23
Mahmood <mahmood19971@live.com>
2024-07-16 10:08:16 +0200

    Konstruktor

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -9,17 +9,31 @@ public class App {
 
         // cat --> Referenzvariable
         // Cat --> Referenztyp(Klasse)
-        Cat cat = new Cat();
+        Cat cat = new Cat("Grizabella","White ", 27);
         output("Blick von aussen: " + cat);
         output("Blick von innen: " + cat.getInstanceVariable());
 
+
+        
+        output(cat.name);
+        output(cat.furColor);
+        output(Integer.toString(cat.age));
+
+
         System.out.println("----------------------------");
 
-        Cat cat2 = new Cat();
+        Cat cat2 = new Cat("Alonzo", "Grey", 35);
         output("Blick von aussen: " + cat2);
         output("Blick von innen: " + cat2.getInstanceVariable());
 
+
+        output(cat2.name);
+        output(cat2.furColor);
+        output(Integer.toString(cat2.age));
     }
+
+       
+
 
       // Statische Methode, die in der Klasse ausgeführt wird ...
     public static void output(String outputStr){
--- com/cc/java/Cat.java
+++ com/cc/java/Cat.java
@@ -2,6 +2,20 @@ package com.cc.java;
 
 public class Cat 
 {
+    String name = "Grizabella";
+    String furColor = "white"; 
+    int age = 27; 
+
+
+    
+    public Cat(String name, String furColor, int age) {
+        this.name = name;
+        this.furColor = furColor;
+        this.age = age;
+    }
+
+
+
     // Rückgabetyp -- Cat
     public Cat getInstanceVariable(){
         return this;
